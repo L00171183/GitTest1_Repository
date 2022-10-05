8b6e2b0bd794e85e86de1438e11f2f33f8cb7551
Priya Kaviyil <L00171183@atu.ie>
2022-10-05 19:33:55 +0530

    Added HelloATU.java

---
+++ HelloAtu1.java
@@ -0,0 +1,10 @@
+public class HelloATU{
+
+	public static void main(string[] args){
+	
+		//prints the text given
+	
+		System.out.println("Hello, Priya from ATU");
+
+ 		}
+	}
---
+++ LICENCE.md
@@ -0,0 +1,2 @@
+#This is my Licence file
+#copyright issued by Priya
